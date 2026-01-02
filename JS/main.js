01042495307a24b25d7a37a7d1e23b7185b1e6a4
Zaid zizoune <zaidzizoune@gmail.com>
2026-01-02 22:40:25 +0100

    feat: add professional product slider to home page

--- JS/main.js
+++ JS/main.js
@@ -2,3 +2,16 @@ let category_nav_list = document.querySelector(".category_nav_list");
 function Open_Categ_list(){
     category_nav_list.classList.toggle("active")
 }
+
+
+  var swiper = new Swiper(".slide_swp", {
+      pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+        clickable: true,
+      },
+      autoplay: {
+        delay: 3000,
+      },
+      loop:true,
+    });
