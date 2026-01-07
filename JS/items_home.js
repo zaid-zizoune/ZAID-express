5dcf13347ca91f8b60bf89c229e097e2f2d3d995
Zaid zizoune <zaidzizoune@gmail.com>
2026-01-07 18:24:05 +0100

    feat: expand home page with category-based product sliders and promotional banners

--- JS/items_home.js
+++ JS/items_home.js
@@ -3,6 +3,12 @@ fetch ('products.json')
 .then (data => {console.log(data);
 
     const swiper_items_sale = document.getElementById("swiper_items_sale");
+  
+    const swiper_electronics = document.getElementById("swiper_electronics");
+
+    const swiper_appliances = document.getElementById("swiper_appliances");
+
+    const swiper_mobiles = document.getElementById("swiper_mobiles");
 
 
 
@@ -55,4 +61,185 @@ fetch ('products.json')
 
     }
 })
+
+    data.forEach(product => {
+        if(product.catetory == "electronics"){
+
+          const old_price_Pargraph = product.old_price ? `<p class="old_price">$${product.old_price}</p>` : '';
+          const percent_dicsc_div = product.old_price ? ` <span class="sale_present">%${Math.floor(((product.old_price - product.price) / product.old_price) * 100)}</span>` : '';
+
+
+
+
+
+
+     swiper_electronics.innerHTML += `
+
+            
+              
+               <div class="swiper-slide product">
+              ${percent_dicsc_div}
+              <div class="img_product">
+                <a href="#"><img src="${product.img}" alt="" /></a>
+              </div>
+
+              <div class="stars">
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+              </div>
+
+              <p class="name_product">
+                <a href="#">${product.name}</a>
+              </p>
+              <div class="price">
+                <p><span>$${product.price}</span></p>
+                ${old_price_Pargraph}
+              </div>
+              <div class="icons">
+                <span class="btn_add_cart">
+                  <i class="fa-solid fa-cart-shopping"></i>add to cart
+                </span>
+                <span class="icon_product"
+                  ><i class="fa-regular fa-heart"></i
+                ></span>
+              </div>
+            </div>
+            
+            
+            
+            
+            
+            `;
+
+
+
+
+
+        }
+})
+
+  data.forEach(product => {
+        if(product.catetory == "appliances"){
+
+          const old_price_Pargraph = product.old_price ? `<p class="old_price">$${product.old_price}</p>` : '';
+          const percent_dicsc_div = product.old_price ? ` <span class="sale_present">%${Math.floor(((product.old_price - product.price) / product.old_price) * 100)}</span>` : '';
+
+
+
+
+
+
+     swiper_appliances.innerHTML += `
+
+            
+              
+               <div class="swiper-slide product">
+              ${percent_dicsc_div}
+              <div class="img_product">
+                <a href="#"><img src="${product.img}" alt="" /></a>
+              </div>
+
+              <div class="stars">
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+              </div>
+
+              <p class="name_product">
+                <a href="#">${product.name}</a>
+              </p>
+              <div class="price">
+                <p><span>$${product.price}</span></p>
+                ${old_price_Pargraph}
+              </div>
+              <div class="icons">
+                <span class="btn_add_cart">
+                  <i class="fa-solid fa-cart-shopping"></i>add to cart
+                </span>
+                <span class="icon_product"
+                  ><i class="fa-regular fa-heart"></i
+                ></span>
+              </div>
+            </div>
+            
+            
+            
+            
+            
+            `;
+
+
+
+
+
+        }
+})
+
+ data.forEach(product => {
+        if(product.catetory == "mobiles"){
+
+          const old_price_Pargraph = product.old_price ? `<p class="old_price">$${product.old_price}</p>` : '';
+          const percent_dicsc_div = product.old_price ? ` <span class="sale_present">%${Math.floor(((product.old_price - product.price) / product.old_price) * 100)}</span>` : '';
+
+
+
+
+
+
+     swiper_mobiles.innerHTML += `
+
+            
+              
+               <div class="swiper-slide product">
+              ${percent_dicsc_div}
+              <div class="img_product">
+                <a href="#"><img src="${product.img}" alt="" /></a>
+              </div>
+
+              <div class="stars">
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+              </div>
+
+              <p class="name_product">
+                <a href="#">${product.name}</a>
+              </p>
+              <div class="price">
+                <p><span>$${product.price}</span></p>
+                ${old_price_Pargraph}
+              </div>
+              <div class="icons">
+                <span class="btn_add_cart">
+                  <i class="fa-solid fa-cart-shopping"></i>add to cart
+                </span>
+                <span class="icon_product"
+                  ><i class="fa-regular fa-heart"></i
+                ></span>
+              </div>
+            </div>
+            
+            
+            
+            
+            
+            `;
+
+
+
+
+
+        }
+})
+
+
+
+
 })
